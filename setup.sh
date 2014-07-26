8686ae56a7e75f751079be691a5e80b2264036ef
Otto Hockel <hockel.otto@googlemail.com>
2014-07-26 08:45:59 +0200

    line endings ...

--- setup.sh
+++ setup.sh
@@ -1,3 +1,3 @@
 # get bash completion for git
-git config --global core.autocrlf true
+git config --global core.autocrlf input
 wget https://raw.github.com/git/git/master/contrib/completion/git-completion.bash
